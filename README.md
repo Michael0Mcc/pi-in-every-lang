d63af993ca1f03863a303f69c40034f154124e17
Michael McCarthy <thmichael300@gmail.com>
2020-02-25 17:25:14 -0600

    Initial commit + Added markup + Finished rust calculation

---
+++ README.md
@@ -0,0 +1,8 @@
+# Calculating Pi in Every Programming Language
+This repository is an attemp to approximate pi in as many programming languages as I can. I will be using the [Leibniz formula for pi](https://en.wikipedia.org/wiki/Leibniz_formula_for_%CF%80 "Leibniz formula for π").
+## Leibniz Formula for π
+```
+1 - 1/3 + 1/5 - 1/7 + 1/9 - ··· = π/4
+```
+## Languages Completed
+- Rust
---
+++ rust-pi.rs
@@ -0,0 +1,15 @@
+//pub const PI: f64 = 3.14159265358979323846264338327950288f64;
+
+fn main() {
+    let mut pi: f64 = 4.0;
+    let mut div: f64 = 1.0;
+    for i in 0..std::usize::MAX {
+        div += 2.0;
+        if i % 2 == 0 {
+            pi -= 4.0/div;
+        } else {
+            pi += 4.0/div;
+        }
+        println!("approx: {},\t % err: {}", pi, (pi - std::f64::consts::PI).abs() / std::f64::consts::PI);
+    }
+}
